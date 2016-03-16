33da1aa083de377c269b5608319d64d478c4f4eb
DerPixler <rene@die-pixler.de>
2016-03-16 19:20:38 +0100

    fix return

--- src/Model/PostMeta.php
+++ src/Model/PostMeta.php
@@ -23,12 +23,13 @@ class PostMeta {
 		 * @return string meta value
 		 */
 		$mk = 'seminar_meta_';
+		$pid = $post->ID;
 
-		$_meta = function( $m, $pid ) use( $mk ) {
-			$pm = get_post_meta( $pid, $mk, TRUE);
-			( is_string( $pm ) || is_int( $pm ) ) ? $v = $pm : $v = ( array_key_exists( $mk . $m, $pm ) ? $pm[$mk . $m][$m] : $pm[$m] );
+		$_meta = function( $m ) use( $mk, $pid ) {
+			$pm = get_post_meta( $pid, $mk . $m, TRUE);
+			return ( is_string( $pm ) || is_int( $pm ) ) ? $v = $pm : $v = ( array_key_exists( $mk . $m, $pm ) ? $pm[$mk . $m][$m] : $pm[$m] );
 		};
-
+		
 		$subtitle = $_meta( 'subtitle', $post->ID );
 		$profile = $_meta( 'profile', $post->ID );
 
